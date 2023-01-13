7883bae6da9becbac99e00e7dce5c05fa269e38e
Sushmita Singh <sushmitasachan97@gmail.com>
2023-01-13 14:26:55 +0530

    First Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+
+        //taking input
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+
+        obj.setName(name);
+        System.out.print(obj.getName());
+
+    }
 }
